8c59f52811fe4cd25921b4c0ea4c9e44e95d05d6
Joninho Rafinha <a23478@alunos.ipca.pt>
2023-01-09 18:19:59 +0000

    First Commit and Layout

---
+++ app/src/main/java/ipca/DJPM/trabalhomovel/MainActivity.kt
@@ -0,0 +1,11 @@
+package ipca.DJPM.trabalhomovel
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.layout_menu)
+    }
+}
